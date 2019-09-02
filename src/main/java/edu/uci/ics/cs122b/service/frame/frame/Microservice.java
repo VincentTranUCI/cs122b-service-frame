2c7edfffc3b37d15d4e98f5b0d8d2a3f4c0ab705
VincentTranUCI <49216305+VincentTranUCI@users.noreply.github.com>
2019-09-02 09:35:20 -0700

    Add files via upload

--- src/main/java/edu/uci/ics/cs122b/service/frame/frame/Microservice.java
+++ src/main/java/edu/uci/ics/cs122b/service/frame/frame/Microservice.java
@@ -14,8 +14,8 @@ import static edu.uci.ics.cs122b.service.frame.frame.Endpoint.MAPPER;
 public class Microservice {
 
     // Used for communication between microservices; takes a request model, a response class, path information,
-    // and a description; returns a response model
-    public static ResponseModel request(RequestModel requestModel, Class<? extends ResponseModel> responseClass,
+    // and a description, sends a POST request; returns a response model
+    public static ResponseModel requestPost(RequestModel requestModel, Class<? extends ResponseModel> responseClass,
                                         String servicePath, String endpointPath, String desc) {
         ServiceLogger.LOGGER.info("Sending request to " + desc + "...");
 
@@ -41,6 +41,43 @@ public class Microservice {
         try {
             String jsonText = response.readEntity(String.class);
             ResponseModel responseModel = MAPPER.readValue(jsonText, responseClass);
+            ServiceLogger.LOGGER.info("Successfully mapped response to POJO.");
+            return responseModel;
+        } catch (IOException e) {
+            ServiceLogger.LOGGER.warning("Unable to map response to POJO.");
+            return null;
+        }
+    }
+
+
+    // Used for communication between microservices; takes a request model, a response class, path information,
+    // and a description, sends a GET request; returns a response model
+    public static ResponseModel requestGet(Class<? extends ResponseModel> responseClass, String servicePath, String endpointPath, String desc) {
+        ServiceLogger.LOGGER.info("Sending request to " + desc + "...");
+
+        // Create a new Client
+        ServiceLogger.LOGGER.info("Building client...");
+        Client client = ClientBuilder.newClient();
+        client.register(JacksonFeature.class);
+
+        // Create a WebTarget to send a request at
+        ServiceLogger.LOGGER.info("Building WebTarget...");
+        WebTarget webTarget = client.target(servicePath).path(endpointPath);
+
+        // Create an InvocationBuilder to create the HTTP request
+        ServiceLogger.LOGGER.info("Starting invocation builder...");
+        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+
+        // Send the request and save it to a Response
+        ServiceLogger.LOGGER.info("Sending request...");
+        Response response = invocationBuilder.get();
+        ServiceLogger.LOGGER.info("Request sent.");
+
+        ServiceLogger.LOGGER.info("Received status " + response.getStatus());
+        try {
+            String jsonText = response.readEntity(String.class);
+            System.out.println(jsonText);
+            ResponseModel responseModel = Endpoint.MAPPER.readValue(jsonText, responseClass);
             ServiceLogger.LOGGER.info("Successfully mapped response to POJO.");
             return responseModel;
         } catch (IOException e) {
